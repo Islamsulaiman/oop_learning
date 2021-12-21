53679c536fdf8bfd578e2a37551c3aac68935274
islam Sulaiman <ma.eslam@yahoo.com>
2021-12-21 18:03:26 +0300

    nested objects

--- oop.js
+++ oop.js
@@ -1,17 +1,22 @@
 // Defining objects
 //  1) object literals
 
-let literals = {
+let user = {
   firstName: "islam",
   lastName: "Sulaiman",
   age: "25",
-  getFullName: () => {
-    return `${literals.firstName} ${literals.lastName}`;
+  location: {
+    egypt: "cairo",
+    KSA: "khobar",
+    mainLocation: () => {
+      return `Your main city is ${user.location.egypt}`;
+    },
   },
-  getAge: () => {
-    return literals.age;
+  getFullName: () => {
+    return `${user.firstName} ${user.lastName}`;
   },
 };
 
-console.log(literals.firstName);
-console.log(literals.getFullName());
+console.log(user.firstName);
+console.log(user.getFullName());
+console.log(user.location.mainLocation());
