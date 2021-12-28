0def6fed70e42d0df4d60a6e03253f7474412594
Islam Sulaiman <ma.eslam@yahoo.com>
2021-12-28 15:21:35 +0300

    constructors

---
+++ constructos.js
@@ -0,0 +1,9 @@
+function Phone(name, price, year) {
+  this.name = name;
+  this.price = price;
+  this.year = year;
+}
+let p1 = new Phone("note 8", 2000, 2019);
+let p2 = new Phone("note 8", 2000, 2019);
+
+console.log(p1);
